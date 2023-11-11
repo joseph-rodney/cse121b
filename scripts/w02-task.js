5e919ee6e52922a8307b4498ca2fbe2521fc17f1
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-11 23:36:17 +0300

    turn to single quotes

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,23 +4,23 @@
 
 /* Step 2 - Variables */
 // Author's name
-let fullName = "Joseph Kaneene";
+let fullName = 'Joseph Kaneene';
 
 // Get current year
 let currentYear = new Date().getFullYear();
 
 // Author's profile picture
-let profilePicture = "images/Profile.jpg";
+let profilePicture = 'images/Profile.jpg';
 
 /* Step 3 - Element Variables */
 
-const nameElement = document.getElementById("name");
+const nameElement = document.getElementById('name');
 
-const foodElement = document.getElementById("food");
+const foodElement = document.getElementById('food');
 
-const yearElement = document.getElementById("#year")
+const yearElement = document.getElementById('year')
 
-const imageElement = document.querySelector("img");
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 
@@ -28,38 +28,39 @@ nameElement.innerHTML = <strong>${fullName}</strong>;
 
 yearElement.textContent = currentYear;
 
-imageElement.setAttribute ("src", profilePicture);
+imageElement.setAttribute ('src', profilePicture);
 
-imageElement.setAttribute("alt", "Profile image of ${fullname}");
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
 
 /* Step 5 - Array */
 
 // Array storing my favourite food
-let favouriteFood = ["Rice", "Cow Peas", "Chappati", "Rolex", "Chicken", "Beef"];
+let favouriteFood = ['Rice', 'Cow Peas', 'Chappati', 'Rolex', 'Chicken', 'Beef'];
 
 // Modify the HTML element with the id of food to display your favorite foods array. 💡Remember that you set a variable named foodElement in Step 3 to reference the element with the id of food.
-foodElement.textContent = favouriteFood.join(", ");
+foodElement.textContent = favouriteFood.join(', ');
 
 // Another single favourite food
-let additionalFavouriteFood = "Kikomando";
+let additionalFavouriteFood = 'Kikomando';
 
 // add additionalFavouriteFood to favouriteFood
 favouriteFood.push(additionalFavouriteFood);
 
 // Append the new array
-foodElement.innerHTML += favouriteFood.join("<br>");
+foodElement.innerHTML += favouriteFood.join('<br>');
 
 // remove the first element in the food array
 favouriteFood.shift();
 
 // Upated version of the array after the removal of the first element
-foodElement.innerHTML += favouriteFood.join("<br>");
+foodElement.innerHTML += favouriteFood.join('<br>');
 
 // Remove the last element of the array
 favouriteFood.pop();
 
 // Updated version of the array after the removal of the last element
-foodElement.innerHTML += favouriteFood.join("<br>");
+foodElement.innerHTML += favouriteFood.join('<br>');
 
 
 

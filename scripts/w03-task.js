e40f868d30eb31506212c43919dcd825445abce2
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-19 06:00:44 +0300

    added the decision structure

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -15,9 +15,41 @@ var subtract = function (number1, number2){
 var multiply = (number1, number2) => number1 * number2;
 
 /* Open Function Use - Divide Numbers */
-
+function divide(number1, number2){
+    return number1 / number2;
+}
 
 /* Decision Structure */
+/* Subtraction Feature */
+function subtractNumbers() {
+    var subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    var subtractNumber2 = Number(document.querySelector('#subtract2').value);
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+}
+
+/* Step 4 - Arrow Functions */
+/* Multiplication Feature */
+var multiplyNumbers = () => {
+    var factor1 = Number(document.querySelector('#factor1').value);
+    var factor2 = Number(document.querySelector('#factor2').value);
+    document.querySelector('#product').value = multiply(factor1, factor2);
+};
+
+/* Step 5 - Function Declaration */
+/* Division Feature */
+function divideNumbers() {
+    var dividend = Number(document.querySelector('#dividend').value);
+    var divisor = Number(document.querySelector('#divisor').value);
+    document.querySelector('#quotient').value = divide(dividend, divisor);
+}
+
+/* Step 6 - Selection Structures */
+document.getElementById('getTotal').addEventListener('click', function () {
+    var subtotal = Number(document.getElementById('subtotal').value);
+    var discount = document.getElementById('member').checked ? 0.15 : 0;
+    var total = subtotal - subtotal * discount;
+    document.getElementById('total').textContent = `$ ${total.toFixed(2)}`;
+});
 
 
 /* ARRAY METHODS - Functional Programming */
